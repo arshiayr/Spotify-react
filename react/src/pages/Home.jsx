06782046763d5b26494796a7ff529108c98dbeaa
arshiayr <ramtinyt55@gmail.com>
2026-08-11 01:49:02 +0330

    add mock tracks and artists to Home page

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,3 +1,96 @@
+import TrackCard from "../components/TrackCard";
+import ArtistCard from "../components/ArtistCard";
+
+const tracks = [
+  {
+    title: "Loser",
+    artist: "Tame Impala",
+    albumId: "5WulAOx9ilWy1h8UGZ1gkI",
+    trackId: "7bxaFZ1O3cHkgLKMsdC3xR",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+    isExplicit: true,
+  },
+  {
+    title: "Loser",
+    artist: "Tame Impala",
+    albumId: "5WulAOx9ilWy1h8UGZ1gkI",
+    trackId: "7bxaFZ1O3cHkgLKMsdC3xR",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+    isExplicit: true,
+  },
+  {
+    title: "Loser",
+    artist: "Tame Impala",
+    albumId: "5WulAOx9ilWy1h8UGZ1gkI",
+    trackId: "7bxaFZ1O3cHkgLKMsdC3xR",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+    isExplicit: true,
+  },
+  {
+    title: "Loser",
+    artist: "Tame Impala",
+    albumId: "5WulAOx9ilWy1h8UGZ1gkI",
+    trackId: "7bxaFZ1O3cHkgLKMsdC3xR",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+    isExplicit: true,
+  },
+  {
+    title: "Loser",
+    artist: "Tame Impala",
+    albumId: "5WulAOx9ilWy1h8UGZ1gkI",
+    trackId: "7bxaFZ1O3cHkgLKMsdC3xR",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+    isExplicit: true,
+  },
+];
+
+const artists = [
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+  {
+    name: "Tame Impala",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02208500450dcd0fd294d7bd3b",
+  },
+];
 
 const Home = () => {
   return (
@@ -15,7 +108,15 @@ const Home = () => {
           </a>
         </div>
 
-        
+        <div className=" flex p-8">
+          {tracks.map((track, index) => (
+            <TrackCard
+              key={index}
+              track={track}
+              onPlay={() => console.log("Playing")}
+            />
+          ))}
+        </div>
       </section>
 
       <section aria-label="Popular artists">
@@ -31,7 +132,15 @@ const Home = () => {
           </a>
         </div>
 
-        
+        <div className=" flex p-8">
+          {artists.map((track, index) => (
+            <ArtistCard
+              key={index}
+              track={track}
+              onPlay={() => console.log("Playing")}
+            />
+          ))}
+        </div>
       </section>
     </>
   );
